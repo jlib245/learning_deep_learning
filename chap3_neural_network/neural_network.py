f316e1511ac5b2d8b534978c7d6dc94bfeae29cc
jlib245 <jlib245@naver.com>
2024-12-19 02:38:02 +0900

    add sigmoid_function

--- chap3_neural_network/neural_network.py
+++ chap3_neural_network/neural_network.py
@@ -11,10 +11,24 @@ def step_function(x) :
 # step_function overloading with numpy
 def step_function(x) :
     y = x > 0
-    return y.astype(int)
-
+    return y.astype(int) # 원하는 자료형
+'''
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1) # y축 범위 지정
+plt.show()
+'''
+
+# sigmoid_function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.array([-1.0, 1.0, 2.0])
+print(sigmoid(x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x,y)
+plt.ylim(-0.1, 1.1)
 plt.show()
