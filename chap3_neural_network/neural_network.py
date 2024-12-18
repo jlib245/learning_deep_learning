a368a057c0dc3d661e9ff80b2c924a61ffc396ca
jlib245 <jlib245@naver.com>
2024-12-19 02:49:55 +0900

    add relu_function

--- chap3_neural_network/neural_network.py
+++ chap3_neural_network/neural_network.py
@@ -24,6 +24,7 @@ plt.show()
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+'''
 x = np.array([-1.0, 1.0, 2.0])
 print(sigmoid(x))
 
@@ -32,3 +33,9 @@ y = sigmoid(x)
 plt.plot(x,y)
 plt.ylim(-0.1, 1.1)
 plt.show()
+'''
+
+# relu_function
+def relu(x) :
+    return np.maximum(0, x)
+
