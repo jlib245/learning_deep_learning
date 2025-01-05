1d28a4bc5318b576cce27c54ee3d52b9f23dd483
jlib245 <jlib245@naver.com>
2025-01-05 17:46:49 +0900

    add backpropagation

--- chap5_backpropagation.py
+++ chap5_backpropagation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from chap3_neural_network import softmax
-from chap4_neural_network_training import cross_entropy_error
+from chap4_neural_network_training import cross_entropy_error, numerical_gradient
 
 class MulLayer:
     def __init__(self):
@@ -98,3 +98,69 @@ class Softmax_with_Loss:
         batch_size = self.t.shape[0]
         dx = (self.y - self.t) / batch_size 
         return dx
+    
+from collections import OrderedDict
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        # 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.lastLayer = Softmax_with_Loss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+    
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.lastLayer.forword(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1 :
+            t = np.argmax(t, axis=1)
+        accuacy = np.sum(y == t) / float(x.shape[0])
+        return accuacy
+    
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+    
+    def gradient(self, x, t):
+        # foward propagation
+        self.loss(x, t)
+
+        # back propagation
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+        
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+
+        return grads
