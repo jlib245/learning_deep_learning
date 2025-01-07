cd8029c7aec241098a24cd6d28359179b3ac90c5
jlib245 <jlib245@naver.com>
2025-01-07 21:26:17 +0900

    add Dropout layer

--- common/layer.py
+++ common/layer.py
@@ -1,3 +1,36 @@
+import numpy as np
+from functions import cross_entropy_error, softmax
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+        return out
+
+    def backward(self, dout):
+        # x와 y를 변경
+        dx = dout * self.y 
+        dy = dout * self.x
+        return dx, dy
+    
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+    
 class Relu:
     def __init__(self):
         self.mask = None
@@ -12,3 +45,70 @@ class Relu:
         dout[self.mask] = 0
         dx = dout
         return dx
+    
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+    
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+    
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.y = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis = 0)
+        return dx
+    
+
+class Softmax_with_Loss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None # one-hot table
+    
+    def forword(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size 
+        return dx
+    
+class Dropout:
+    def __init__(self, dropout_ratio = 0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+        
+    def forward(self, x, train_flg = True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask
+        else :
+            return x * (1.0 - self.dropout_ratio)
+        
+    def backward(self, dout):
+        return dout * self.mask
